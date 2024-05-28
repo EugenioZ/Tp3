0f130b9b0249f3de01ba975af204b453483f492a
EugenioZ <110923362+EugenioZ@users.noreply.github.com>
2024-05-28 11:37:37 -0300

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+
+variable "container_image_url" {
+  type        = string
+  description = "The url of the compute containter image"
+}
+
+variable "lab_role_arn" {
+  type        = string
+  description = "The arn of your lab role user"
+}
